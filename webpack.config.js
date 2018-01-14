30b11eb7fcedd86de1023c20396f725c8c99c592
Klaus Fischer <klausfisc@gmail.com>
2018-01-14 20:28:30 +0100

    Add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const config = (env) => ({
+  entry: {
+    reely: './src/reely.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+  mode: 'production',
+});
+
+module.exports = config;
